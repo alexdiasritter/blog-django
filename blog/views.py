07869985670543bed858451f1ec13ddd578d4e2c
AlexRitter1 <ritter.alex@hotmail.com>
2025-10-23 14:28:41 -0300

    feat: paginação, class-based view e formulário para envio de email adicionados.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,55 @@
 from django.shortcuts import get_object_or_404, render
+from .forms import EmailPostForm
 from .models import Post
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.views.generic import ListView
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request, 'blog/post/list.html',{'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+    Post,
+    id=post_id,
+    status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+        'post': post,
+        'form': form
+        }
+    )
